b405c876506bf4203007aedc729c7abc734bdf74
matheusslb <matheussantosdelimabrito@gmail.com>
2025-04-25 15:25:36 -0300

    projeto inicial

---
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.SenaiCommunity.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
