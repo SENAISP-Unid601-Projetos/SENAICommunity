3573834c74b1fa2588171eb3235232bd79ec0e3c
GabrielPiscke <gabrielmateuspiscke@gmail.com>
2025-04-25 17:07:35 -0300

    UpdBiel

--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Aluno.java
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Aluno.java
@@ -1,4 +1,33 @@
 package com.SenaiCommunity.BackEnd.Entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Aluno {
+    private Long id;
+    private String nomeCompleto;
+    private String email;
+    private String senha;
+    private String matricula;
+    private String curso;
+    private String periodo;
+    private String fotoPerfilUrl;
+    private LocalDate dataNascimento;
+    private List<Telefone> telefone;
+    private String statusConta; // Ex: "Ativo", "Inativo"
+    private LocalDate
+            dataCadastro;
+
+    // Relacionamentos (exemplos)
+    private Projeto projeto;
+    private  MensagensChat mensagens;
+
 }
--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Projeto.java
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Projeto.java
@@ -1,4 +1,13 @@
 package com.SenaiCommunity.BackEnd.Entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Projeto {
 }
