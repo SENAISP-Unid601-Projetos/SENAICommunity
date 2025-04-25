c03a0562bdeb7160ab15c06a5e0a8b3568be0a7a
matheusslb <matheussantosdelimabrito@gmail.com>
2025-04-25 17:06:21 -0300

    UpMat

--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Professor.java
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Professor.java
@@ -1,4 +1,39 @@
 package com.SenaiCommunity.BackEnd.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
 public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // Dados pessoais
+    private String nomeCompleto;
+    private String email;
+    private String senha;
+    private String fotoPerfil;
+    private Date dataNascimento;
+    private String bio;
+
+    // Contato
+    @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Telefone> telefones;
+
+    // Acadêmico/profissional
+    private String formacao;
+    private String areaAtuacao;
+
+    // Sistema
+    private String tipoUsuario;
+
+    //Relacionamentos
+    @OneToMany(mappedBy = "professor")
+    private List<Projeto> projetosOrientados;
+
 }
