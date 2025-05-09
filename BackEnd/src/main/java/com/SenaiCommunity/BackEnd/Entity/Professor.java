f01b18aaf8047acea60c01dd3965dd59e81b60ac
matheusslb <matheussantosdelimabrito@gmail.com>
2025-05-09 14:45:13 -0300

    entity Projeto

--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Professor.java
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Professor.java
@@ -1,12 +1,16 @@
 package com.SenaiCommunity.BackEnd.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Professor {
     @Id
--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Projeto.java
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Projeto.java
@@ -1,13 +1,36 @@
 package com.SenaiCommunity.BackEnd.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class Projeto {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        private String titulo;
+        private String descricao;
+        private Date dataInicio;
+        private Date dataEntrega;
+        private String status; //PLANEJADO, EM_ANDAMENTO, CONCLUIDO
+
+        // Relacionamentos
+        @ManyToOne
+        private Professor professor;
+
+        @OneToOne(mappedBy = "projeto")
+        private Grupos grupo;
+
+        @OneToMany(mappedBy = "projeto", cascade = CascadeType.ALL)
+        private List<Avaliacoes> avaliacoes;
+
 }
