0718036d215c9705121fc13b5f318f5e801fe660
MykyBalb <mykaelbalbino06@gmail.com>
2025-05-16 15:03:55 -0300

    convite

--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Convite.java
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/Convite.java
@@ -1,9 +1,33 @@
 package com.SenaiCommunity.BackEnd.Entity;
 
-import jakarta.persistence.Entity;
+import com.SenaiCommunity.BackEnd.Enum.StatusConvite;
+import jakarta.persistence.*;
 import lombok.Data;
 
-@Data
+import java.time.LocalDate;
+
 @Entity
+@Data
 public class Convite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idConvite;
+
+    @ManyToOne
+    @JoinColumn(name = "id_remetente", nullable = false)
+    private Usuario remetente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_destinatario", nullable = false)
+    private Usuario destinatario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_grupo", nullable = false)
+    private Grupos grupo;
+
+    @Enumerated(EnumType.STRING)
+    private StatusConvite status;
+
+    private LocalDate dataConvite;
 }
