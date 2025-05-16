d50d3de490d7b91c59eee40dbfd777fd8096c57a
GabrielPiscke <gabrielmateuspiscke@gmail.com>
2025-05-16 14:31:41 -0300

    UpdEntityChatPrivado

--- BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/ChatPrivado.java
+++ BackEnd/src/main/java/com/SenaiCommunity/BackEnd/Entity/ChatPrivado.java
@@ -1,9 +1,26 @@
 package com.SenaiCommunity.BackEnd.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
 public class ChatPrivado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL)
+    private List<MensagemPrivada> mensagens;
 }
